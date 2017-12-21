bccac270ddad5fa9f8dfa9bca18d7d16efc26c55
cmirza <mail@cmirza.com>
2017-12-21 12:46:31 -0800

    Initial commit

--- mainsite/FreshPoint/admin.py
+++ mainsite/FreshPoint/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vegetable, State, Season
+
+admin.site.register(Vegetable)
+admin.site.register(State)
+admin.site.register(Season)
